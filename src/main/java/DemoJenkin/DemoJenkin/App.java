cc5e26163095eee44f1a464d0b1ec37145823253
Sapna Joshi <sapan030717@gmail.com>
2020-06-11 17:46:13 -0500

    demo

---
+++ src/main/java/DemoJenkin/DemoJenkin/App.java
@@ -0,0 +1,13 @@
+package DemoJenkin.DemoJenkin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/DemoJenkin/DemoJenkin/HomeTest.java
@@ -0,0 +1,41 @@
+package DemoJenkin.DemoJenkin;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class HomeTest {
+	WebDriver driver;
+	@BeforeMethod
+	public void SetUp() {
+	
+		 System.setProperty("webdriver.chrome.driver","C:\\Users\\sapna\\Desktop\\selenium\\chromedriver_win32\\chromedriver.exe");
+		 driver=new ChromeDriver();
+		 driver.manage().window().maximize();
+			driver.manage().deleteAllCookies();
+			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS );
+			driver.get("https://www.facebook.com/");
+		 
+	}
+	
+	@Test
+	public void VerifyHomeTitleTest() {
+		String title=driver.getTitle();
+		System.out.println(title);
+		Assert.assertEquals(title,"Facebook - Log In or Sign Up","Home page title doesn't Match");
+	}
+	
+	@AfterMethod
+	public void TearDown() {
+		driver.close();
+	}
+
+
+}
